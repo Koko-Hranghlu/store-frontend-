b0f900b23e7304903ba71f2ee5a7015ef346a757
Hapari Paya <94343756+Koko-Hranghlu@users.noreply.github.com>
2025-03-06 18:57:34 +0530

    Update formatCurrency.js

--- util/formatCurrency.js
+++ util/formatCurrency.js
@@ -1,1 +1,8 @@
+const formatter = new Intl.NumberFormat(undefined, {
+  style: "currency",
+  currency: "USD"
+})
 
+export default function formatCurrency(amount) {
+  return formatter.format(amount).slice(2)
+}
